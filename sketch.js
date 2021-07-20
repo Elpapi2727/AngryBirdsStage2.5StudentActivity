81804781c35404a8c28db4088e904a8a0559bf0d
Elpapi2727 <85907374+Elpapi2727@users.noreply.github.com>
2021-07-20 17:51:59 -0500

    Chain bird

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -26,16 +27,18 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
+    box5 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
+    box6 = new Box(810,160,70,70);
+    
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,box6.body)
 }
 
 function draw(){
@@ -52,13 +55,16 @@ function draw(){
 
     box3.display();
     box4.display();
+    box5.display();
     pig3.display();
     log3.display();
 
-    box5.display();
+    box6.display();
     log4.display();
     log5.display();
 
     bird.display();
     platform.display();
+
+    chain.display();
 }
