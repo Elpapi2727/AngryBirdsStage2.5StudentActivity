b878ba67807d3d9f63903bbc828b041b5b74987f
Elpapi2727 <85907374+Elpapi2727@users.noreply.github.com>
2021-07-22 17:40:30 -0500

    Resorte

--- sketch.js
+++ sketch.js
@@ -27,18 +27,16 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    box5 = new Box(920,240,70,70);
+    box5 = new Box(810,160,70,70);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
-    box6 = new Box(810,160,70,70);
     
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    chain = new Chain(bird.body,box6.body)
+    slingshot = new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -59,12 +57,20 @@ function draw(){
     pig3.display();
     log3.display();
 
-    box6.display();
     log4.display();
     log5.display();
 
     bird.display();
     platform.display();
 
-    chain.display();
+    slingshot.display();
+}
+
+function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+
+}
+
+function mouseReleased(){
+slingshot.fly();
 }
